1c537592ab9ed00a90d437ffd259e0047b258ba4
NavidKalashi <kalashinavid@gmail.com>
2024-03-11 15:46:04 +0330

    first commit

---
+++ ListAndArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ListAndArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3};
+            Console.WriteLine(numbers);
+        }
+    }
+}
